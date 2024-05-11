02afc5f6cf48031b05ff867b4a9a7d2634026183
angel <aced8800@students.kleinisd.net>
2024-05-11 01:21:43 +0000

    chore: Refactor calculator code and add multiplication and division operations

--- c/calc.c
+++ c/calc.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-int sum(int num1, int num2) {
+int add(int num1, int num2) {
     return num1 + num2;
 }
 
@@ -10,11 +10,59 @@ int sub(int num1, int num2) {
     return num1 - num2;
 }
 
+int mul(int num1, int num2) {
+    return num1 * num2;
+}
+
+int divide(int num1, int num2) {
+    return num1 / num2;
+}
+
 int main() {
+    // Print the title
     printf("Angel's Basic Calculator ");
     printf("\xC2\xA9 2024 \n");
+
+    // Get the type of operation
     printf("Enter Type? \n");
-    char type;
-    scanf("%c", &type);
+
+    // Print the options
+    printf("1. Addition \n");
+    printf("2. Subtraction \n");
+    printf("3. Multiplication \n");
+    printf("4. Division \n");
+    
+    // Get User input for the type of operation
+    int type; 
+    scanf("%d", &type);
+
+    // Get User input for the numbers
+    int num1, num2;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    // Perform the operation and print the result
+    int result;
+    switch(type) {
+        case 1:
+            result = add(num1, num2);
+            break;
+        case 2:
+            result = sub(num1, num2);
+            break;
+        case 3:
+            result = mul(num1, num2);
+            break;
+        case 4:
+            result = divide(num1, num2);
+            break;
+        default:
+            printf("Invalid operation type.\n");
+            return 1;
+    }
+    printf("Result: %d\n", result);
+
     return 0;
 }
