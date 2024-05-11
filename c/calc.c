e51894781fd835f21f70b2efc3f122e36883b85e
aced8800 <aced8800@students.kleinisd.net>
2024-05-10 20:41:29 -0400

    C Calculator

---
+++ c/calc.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int sum(int num1, int num2) {
+    return num1 + num2;
+}
+
+int sub(int num1, int num2) {
+    return num1 - num2;
+}
+
+int main() {
+    printf("Angel's Basic Calculator ");
+    printf("\xC2\xA9 2024 \n");
+    printf("Enter Type? \n");
+    char type;
+    scanf("%c", &type);
+    return 0;
+}
